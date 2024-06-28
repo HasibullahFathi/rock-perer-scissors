1f61e49d187330e772790bf808aceb97b401c00b
HasibullahFathi <hasibullah.fathi@gmail.com>
2024-06-28 14:14:08 +0000

    created startGame function to choose randomly weapons for computer and set pics to both player acordingly

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,23 @@ const weapons = ["Rock", "Paper", "Scissors", "Lizard", "Spack"];
 for(let button of buttons) {
     button.addEventListener("click", function() {
         let playerWeapon = this.getAttribute("data-weapon");
-       
-        console.log(playerWeapon);
+
+        startGame(playerWeapon);
     });
 }
+
+function startGame(playerWeapon) {
+    
+    playerImage.src = `assets/images/${weapons[playerWeapon]}.webp`;
+    playerImage.alt = `weapons ${playerWeapon}`;
+
+    let computerWeapon = Math.floor(Math.random() * 5);
+    computerImage.src = `assets/images/${weapons[computerWeapon]}.webp`;
+    computerImage.alt = `weapons ${weapons[computerWeapon]}`;
+
+    console.log(computerWeapon);
+    console.log(playerWeapon);
+
+}
+
+// startGame(playerWeapon);
